df446c1c8cf87bd66967ebf06f0e4961c1b6bc37
Magnus Wibeck <magnus.wibeck@sato-global.com>
2023-12-08 14:31:02 +0100

    Day 8, pt 1.

---
+++ 08/1.lua
@@ -0,0 +1,35 @@
+#!/usr/local/bin/lua-5.1
+
+args = {...}
+
+local path
+local nodes = {}
+
+for l in io.lines(args[1]) do
+  local n,left,right = l:match("(.*) = %((.*), (.*)%)")
+  print(n,left,right)
+  if left == nil and #l > 2 then
+    path = l
+  elseif n ~= nil then
+    nodes[n] = {L=left, R=right}
+  end
+end
+
+local idx = 1
+local steps = 0
+local node = "AAA"
+local new
+
+repeat
+  dir = path:sub(idx,idx)
+  new = nodes[node][dir]
+
+  print(node, dir, new, idx)
+
+  node = new
+  idx = (idx % #path) + 1
+  steps = steps + 1
+  print(node)
+until new == "ZZZ"
+
+print(steps)
