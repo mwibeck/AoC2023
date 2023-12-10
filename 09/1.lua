bc9089d045d1fbdbf4583fda455689dd148229d8
Magnus Wibeck <mwibeck@mba.local>
2023-12-10 01:21:15 +0100

    Day 9, part 1.

---
+++ 09/1.lua
@@ -0,0 +1,49 @@
+#!/usr/local/bin/lua-5.1
+args = {...}
+
+history = {}
+
+for l in io.lines(args[1]) do
+   local vals = {}
+
+   for val in l:gmatch("-?%d+") do
+      table.insert(vals, tonumber(val))
+   end
+
+   table.insert(history,vals)
+   print(table.concat(vals," "))
+end
+
+local bigsum = 0
+
+for i,vals in ipairs(history) do
+
+   local diffs = {}
+   local allZ
+
+   local curr = {unpack(vals)}
+   local sum = curr[#curr]
+
+   print("current", table.concat(curr,"-"))
+   -- Calc all diffs
+   repeat
+ 
+      local diff = {}
+      allZ = true
+      -- one level
+      for j=1,#curr-1 do
+	 diff[j] = curr[j+1] - curr[j]
+	 allZ = allZ and (diff[j] == 0)
+	 if j==#curr-1 then
+	    sum = sum+diff[j]
+	 end
+      end
+      if not allZ then
+	 table.insert(diffs,diff)
+	 print(table.concat(diff," "))
+	 curr = {unpack(diff)}
+      end
+   until allZ
+   bigsum = bigsum + sum
+end
+print(bigsum)
